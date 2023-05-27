e5a970abd945b54137acb706337564b8dc3444e7
Gideon Low <hello@bygideon.com>
2023-05-27 10:46:52 +0800

    Initial release

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,31 @@
+defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
-  content: ["./src/*.html"],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
       colors: {
-        'daily-dev-tips': "#F89283"
-      }
+        bloodMoon: {
+          100: "#FBEBEB",
+          200: "#F7D6D6",
+          300: "#EFAEAD",
+          400: "#E88584",
+          500: "#E05D5B",
+          600: "#D83432",
+          700: "#AD2A28",
+          800: "#821F1E",
+          900: "#561514",
+          1000: "#2B0A0A",
+        },
+      },
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+        heading: ["GeneralSans", ...defaultTheme.fontFamily.sans],
+      },
     },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
+};
